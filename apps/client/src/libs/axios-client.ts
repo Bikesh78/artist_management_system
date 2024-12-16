ca2061e326ed0abfa2fa3e7070b039cca45fe9fd
bikesh <bikesh>
2024-12-16 13:53:13 +0545

    Set up axios instance

---
+++ apps/client/src/libs/axios-client.ts
@@ -0,0 +1,31 @@
+import Axios, { InternalAxiosRequestConfig } from 'axios';
+
+function authRequestInterceptor(config: InternalAxiosRequestConfig) {
+  if (config.headers) {
+    config.headers.Accept = 'application/json';
+  }
+
+  config.withCredentials = true;
+  return config;
+}
+
+export const axiosInstance = Axios.create({
+  baseURL: import.meta.env.VITE_BASE_URL,
+});
+
+axiosInstance.interceptors.request.use(authRequestInterceptor);
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response.data;
+  },
+  (error) => {
+    const message = error.response?.data?.message || error.message;
+    console.log('error message', message)
+
+    if (error.response?.status === 401) {
+      window.location.href = "/login";
+    }
+
+    return Promise.reject(error);
+  },
+);
